23e9cc3c13bef64dad70e5b0d43b5dab6e5f9d82
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-08-13 22:34:42 +0900

    fix wrong argument type of fill_bits function
    also, change to include stdint.h to use the types

--- fill_bits.h
+++ fill_bits.h
@@ -1,6 +1,8 @@
 #ifndef __FILL_BITS_H_INCLUDED__
 #define __FILL_BITS_H_INCLUDED__
 
-	uint64_t fill_bits(int8_t n);
+#include <stdint.h>
+
+	uint64_t fill_bits(uint8_t n);
 
 #endif /* __FILL_BITS_H_INCLUDED__ */
